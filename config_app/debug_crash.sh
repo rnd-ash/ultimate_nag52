58b02e961a3650ce10e0846e01b41e1fee0d8878
Ashcon Mohseninia <ashconm@outlook.com>
2024-05-09 19:33:53 +0100

    remove dead config app

--- config_app/debug_crash.sh
+++
@@ -1,1 +0,0 @@
-~/.platformio/packages/toolchain-xtensa-esp32/bin/xtensa-esp32-elf-gdb $1/.pio/build/esp-wrover-kit/firmware.elf dump.elf
--- config_app/src/usb_hw/flasher/mod.rs
+++
@@ -1,6 +0,0 @@
-//! ESP Flashing utility in order
-//! to flash new firmware to the TCM
-//!
-//! This modules content is based on https://github.com/espressif/esptool/blob/master/esptool.py
-
-pub mod bin;
